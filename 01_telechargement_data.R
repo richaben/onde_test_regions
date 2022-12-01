da9846da8a9144f180970b7dbc1cbddda12c4f37
Benoit Richard <brichard.rb@gmail.com>
2022-12-01 22:08:20 +0100

    Update 01_telechargement_data.R

--- 01_telechargement_data.R
+++ 01_telechargement_data.R
@@ -28,7 +28,8 @@ library(data.table)
 ### Selection des départements ----
 
 #dpt_sel <- c('14','27','50','61','76')
-dpt_sel <- c('22','29','35','56')
+#dpt_sel <- c('22','29','35','56')
+dpt_sel <- c('60','62','80','59','02')
 
 ### Date du jour ----
 date_jour <- as.character(format(Sys.time(),"%Y-%m-%d"))
